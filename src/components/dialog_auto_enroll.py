cd01bc72c99d90fe3187229e52e9131887bf50cf
Satyam Verma <satyamvr33@gmail.com>
2026-08-15 15:46:26 +0530

    Fix quick enrollment and secure Supabase config

--- src/components/dialog_auto_enroll.py
+++ src/components/dialog_auto_enroll.py
@@ -8,13 +8,14 @@ import time
 def auto_enroll_dialog(subject_code):
     student_id = st.session_state.student_data['student_id']
 
-    res = supabase.table('subjects').select('subject_id, name').eq('subject_code', subject_code)
+    res = supabase.table('subjects').select('subject_id, name').eq('subject_code', subject_code).execute()
     if not res.data:
         st.error('Subject Code not found!')
         if st.button('Close'):
             st.query_params.clear()
             st.rerun()
         return
+
     subject = res.data[0]
     check = supabase.table('subject_students').select('*').eq('subject_id', subject['subject_id']).eq('student_id', student_id).execute()
     if check.data:
@@ -22,10 +23,11 @@ def auto_enroll_dialog(subject_code):
         if st.button('Got it!'):
             st.query_params.clear()
             st.rerun()
-        return 
-    st.markdown(f'WOuld you like to enroll in **{subject['name']}** ?')        
-    
-    col1 , col2 = st.columns(2)
+        return
+
+    st.markdown(f"Would you like to enroll in **{subject['name']}**?")
+
+    col1, col2 = st.columns(2)
 
     with col1:
         if st.button('No thanks'):
@@ -35,7 +37,7 @@ def auto_enroll_dialog(subject_code):
         if st.button('Yes enroll now!', type='primary', width='stretch'):
             enroll_student_to_subject(student_id, subject['subject_id'])
             st.success('Joined successfully!')
-            st.query_params.claer()
+            st.query_params.clear()
             time.sleep(2)
             st.rerun()
 
